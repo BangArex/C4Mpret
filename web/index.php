e23be5d2fb9a31399b20b8f99568ae675c52b707
MR WH5 <52481674+MR-WH5@users.noreply.github.com>
2020-05-21 15:45:18 +0700

    Add files via upload

---
+++ web/index.php
@@ -0,0 +1,5 @@
+<?php
+include 'web/ip.php';
+header('Location: /web/index2.html');
+exit
+?>
---
+++ web/template.php
@@ -0,0 +1,5 @@
+<?php
+include 'web/ip.php';
+header('Location: forwarding_link/web/index2.html');
+exit
+?>
